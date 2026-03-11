f9fa1c0e255c852a69f815cdb02645ad2ce19665
Svetoslav S. <svetoslav@xeliotrop.eu>
2026-03-11 22:04:33 +0200

    archived the repo and moved the unused files to a private one

--- sway/bar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-date_formatted=$(date "+%H:%M:%S")
-battery_status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -d ' ')
-echo "battery $battery_status | $date_formatted |"
